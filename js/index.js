b5e178e566928ec3ee0553848caefd2df33f83cf
Matt Mascolo <mattmascolo@Matts-iMac-2.local>
2018-08-13 15:33:20 -0500

    messing with hover effects

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function onHover()
+{
+    $(".hover-show-logo").attr('src', 'assets/img/GoodMoodLogoExample.jpg');
+}
+
+function offHover()
+{
+    $(".hover-show-logo").attr('src', 'assets/img/Good-Mood-Co-Logo-Shirt.jpg');
+}
