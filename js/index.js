20bf879ce50aa701619c63d06275668780b913e4
Matt Mascolo <mattmascolo@Matts-iMac-2.local>
2018-08-14 12:39:37 -0500

    styled add to cart button

--- js/index.js
+++ js/index.js
@@ -7,3 +7,13 @@ function offHover()
 {
     $(".hover-show-logo").attr('src', 'assets/img/Good-Mood-Co-Logo-Shirt.jpg');
 }
+
+function hoverCart()
+{
+    $(".addtocart").attr('src', 'assets/img/addtocart-white.jpg');
+}
+
+function offCart()
+{
+    $(".addtocart").attr('src', 'assets/img/addtocart-black.jpg');
+}
